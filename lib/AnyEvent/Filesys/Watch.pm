7bc3ff90257981d4a829661d5a756b917a10c04c
Guido Flohr <guido.flohr@cantanea.com>
2023-06-27 10:32:38 +0300

    initializied i18n

--- lib/AnyEvent/Filesys/Watch.pm
+++ lib/AnyEvent/Filesys/Watch.pm
@@ -1,16 +1,28 @@
 #! /bin/false
 
 # This next lines is here to make Dist::Zilla happy.
-# ABSTRACT: Watch file system for changes 
+# ABSTRACT: Watch file system for changes
 
 package AnyEvent::Filesys::Watch;
 
 use strict;
 use v5.10;
 
+use Locale::TextDomain ('AnyEvent-Filesys-Watch');
+
 sub new {
 	my ($class, %args) = @_;
 
+	my @required = qw(dirs cb);
+	foreach my $required (@required) {
+		if (!exists $args{$required}) {
+			require Carp;
+			Carp::croak(
+				__x("Mandatory argument '{arg}' missing",
+				    arg => $required)
+			);
+		}
+	}
 	my $self = {};
 	foreach my $arg (keys %args) {
 		$self->{'__' . $arg} = $args{$arg};
