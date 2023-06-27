7440e1f98f5c324c5f81759a659a1e3187c9d355
Guido Flohr <guido.flohr@cantanea.com>
2023-06-27 12:47:10 +0300

    backend selection logic

--- lib/AnyEvent/Filesys/Watch.pm
+++ lib/AnyEvent/Filesys/Watch.pm
@@ -29,6 +29,57 @@ sub new {
 	}
 
 	bless $self, $class;
+
+	$self->__loadBackend;
+
+	return $self;
+}
+
+sub backend {
+	shift->{__backend};
+}
+
+sub backendClass {
+	shift->{__backend_class};
+}
+
+sub no_external {
+	require Carp;
+	Carp::croak('use noExternal instead of no_external');
+}
+
+sub noExternal {
+	shift->{__no_external};
+}
+
+sub __loadBackend {
+	my ($self) = @_;
+
+	my $backend_class;
+
+	if ($self->backend) {
+		# Use the AEFW::Backend prefix unless the backend starts with a +
+		my $prefix  = "AnyEvent::Filesys::Watch::Backend::";
+		$backend_class = $self->backend;
+		$backend_class = $prefix . $backend_class
+			unless $backend_class =~ s{^\+}{};
+	} elsif ( $self->noExternal ) {
+		$backend_class = "AnyEvent::Filesys::Watch::Backend::Fallback";
+	} elsif ( $^O eq 'linux' ) {
+		$backend_class = 'AnyEvent::Filesys::Watch::Backend::Inotify2';
+	} elsif ( $^O eq 'darwin' ) {
+		$backend_class = "AnyEvent::Filesys::Watch::Backend::FSEvents";
+	} elsif ( $^O =~ /bsd/ ) {
+		$backend_class = "AnyEvent::Filesys::Watch::Backend::KQueue";
+	} else {
+		$backend_class = "AnyEvent::Filesys::Watch::Backend::Fallback";
+	}
+
+	$self->{__backend_class} = $backend_class;
+
+	# TODO! Load the backend!
+
+	return $self;
 }
 
 1;
