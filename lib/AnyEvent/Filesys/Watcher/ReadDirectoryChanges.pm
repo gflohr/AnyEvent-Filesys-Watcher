2bd67b507d05cb062c32bc86d7f82031ca2fea45
Guido Flohr <guido.flohr@cantanea.com>
2023-07-03 08:02:26 +0300

    avoid cyclic reference
    This is crucial because otherwise the destructor of
    Filesys::Notify::Win32::ReadDirectoryChanges is not called.

--- lib/AnyEvent/Filesys/Watcher/ReadDirectoryChanges.pm
+++ lib/AnyEvent/Filesys/Watcher/ReadDirectoryChanges.pm
@@ -5,6 +5,7 @@ use strict;
 use AnyEvent;
 
 use Filesys::Notify::Win32::ReadDirectoryChanges;
+use Scalar::Util qw(weaken);
 
 use base qw(AnyEvent::Filesys::Watcher);
 
@@ -18,16 +19,18 @@ sub new {
 		$watcher->watch_directory(path => $directory, subtree => 1);
 	}
 
+	my $alter_ego = $self;
 	my $timer = AnyEvent->timer(
 		after => $self->interval,
 		interval => $self->interval,
 		cb => sub {
 			if ($watcher->queue->pending) {
 				my @events = $watcher->queue->dequeue;
-				$self->_processEvents(@events);
+				$alter_ego->_processEvents(@events);
 			}
 		}
 	);
+	weaken $alter_ego;
 	if (!$timer) {
 		die __x("Error creating timer: {error}\n", error => $@);
 	}
